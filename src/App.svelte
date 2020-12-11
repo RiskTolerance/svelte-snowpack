93217a35ca9e946b91340d765b657a9fd8c3507e
RiskTolerance <isaac.druin@gmail.com>
2020-12-11 16:48:30 -0600

    converted logo and background svg to html, logo and text change color depending on time of day, fixed pop-in of some assets

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,75 @@
 <!-- App.svelte -->
 <script>
-	import { onMount } from 'svelte';
-	import dayjs from 'dayjs';
-	import mix from 'mix-color';
-	import SocialLinks from './SocialLinks.svelte';
-	import IntroText from './IntroText.svelte';
+	import { onMount } from "svelte";
+	import dayjs from "dayjs";
+	import mix from "mix-color";
+	import SocialLinks from "./SocialLinks.svelte";
+	import IntroText from "./IntroText.svelte";
+	import ForestStreamLogo from "./ForestStreamLogo.svelte";
+	import ForestBackground from "./ForestBackground.svelte";
 
 	let now = dayjs();
-	let hour = now['$H'];
-	let minute = now['$m'];
-	//let dayProgress = (((hour + minute / 60) / 24) * 100).toFixed(0);
+	let hour = now["$H"];
+	let minute = now["$m"];
+	let dayProgress = (((hour + minute / 60) / 24) * 100).toFixed(0);
+	console.log(dayProgress);
 	let hourProgress = (minute / 60).toFixed(2);
 
 	const colorAnchors = [
-		'41,40,61',
-		'42,40,60',
-		'50,43,61',
-		'65,47,63',
-		'90,53,67',
-		'131,62,75',
-		'189,77,85',
-		'206,119,99',
-		'223,193,126',
-		'242,220,137',
-		'250,238,142',
-		'252,248,146',
+		"41,40,61",
+		"42,40,60",
+		"50,43,61",
+		"65,47,63",
+		"90,53,67",
+		"131,62,75",
+		"189,77,85",
+		"206,119,99",
+		"223,193,126",
+		"242,220,137",
+		"250,238,142",
+		"252,248,146",
 		// pass noon
-		'252,248,146',
-		'250,238,142',
-		'242,220,137',
-		'223,193,126',
-		'206,119,99',
-		'189,77,85',
-		'131,62,75',
-		'90,53,67',
-		'65,47,63',
-		'50,43,61',
-		'42,40,60',
-		'41,40,61',
+		"252,248,146",
+		"250,238,142",
+		"242,220,137",
+		"223,193,126",
+		"206,119,99",
+		"189,77,85",
+		"131,62,75",
+		"90,53,67",
+		"65,47,63",
+		"50,43,61",
+		"42,40,60",
+		"41,40,61",
 	];
-	let backgroundColor = `rgb(${colorAnchors[hour]})`;
+	let backgroundColor;
+	const calcBgColor = () => {
+		if (hour < 24) {
+			backgroundColor = mix(
+				`rgb(${colorAnchors[hour]})`,
+				`rgb(${colorAnchors[hour + 1]})`,
+				`${hourProgress}`
+			);
+		} else {
+			backgroundColor = `rgb(${colorAnchors[hour]})`;
+		}
+	};
+	calcBgColor();
+
+	let fillColor = hour < 9 | hour > 14 ? "white" : "black";
+	let textColor = hour < 9 | hour > 14 ? "white" : "black";
 
 	onMount(() => {
 		const interval = setInterval(() => {
 			now = dayjs();
-			hour = now['$H'];
-			minute = now['$m'];
+			hour = now["$H"];
+			minute = now["$m"];
 			hourProgress = (minute / 60).toFixed(2);
-			if (hour < 24) {
-				backgroundColor = mix(
-					`rgb(${colorAnchors[hour]})`,
-					`rgb(${colorAnchors[hour + 1]})`,
-					`${hourProgress}`
-				);
-			} else {
-				backgroundColor = `rgb(${colorAnchors[hour]})`;
-			}
+			calcBgColor();
+			fillColor = hour < 9 | hour > 14 ? "white" : "black";
+			textColor = hour < 9 | hour > 14 ? "white" : "black";
 			//console.log(`The time is ${hour}:${minute} and the day is ${dayProgress}% complete`);
-		}, 1000);
+		}, 10000);
 		return () => {
 			clearInterval(interval);
 		};
@@ -76,30 +88,14 @@
 		font-size: calc(10px + 2vmin);
 		overflow: hidden;
 	}
-	.App-logo {
-		height: 25vmin;
-		width: 80vmin;
-		margin-top: 20px;
-		pointer-events: none;
-		animation: App-logo-pulse infinite 1.6s ease-in-out alternate;
-		
-	}
-	#bg-image {
-		position: absolute;
-		bottom: -100px;
-		margin: 0;
-		width: 110vw;
-		opacity: 50%;
-		z-index: 0;
-		mix-blend-mode: overlay;
-	}
 </style>
 
 <div class="App">
 	<header style="background-color: {backgroundColor}" class="App-header">
-		<img src="/fsslogo.svg" class="App-logo" alt="logo" />
-		<IntroText/>
-		<SocialLinks/>
-		<img id="bg-image" src="/forest.svg" alt="forest background">
+		<!-- <img src="/fsslogo.svg" class="App-logo" alt="logo" /> -->
+		<ForestStreamLogo {fillColor}/>
+		<IntroText {textColor} />
+		<SocialLinks />
+		<ForestBackground />
 	</header>
 </div>
--- src/IntroText.svelte
+++ src/IntroText.svelte
@@ -1,7 +1,8 @@
 <script>
     import { onMount } from 'svelte';
     import { fade } from 'svelte/transition';
-    
+    export let textColor;
+
     let loaded = false;
     onMount(() => {
         setTimeout(() => {
@@ -19,20 +20,15 @@
     color: white;
     width: 60vmin;
     height: 100px;
-    padding: 25px 0;
-    border-bottom: 4px solid white;
+    padding-bottom: 25px;
     font-size: calc(10px + 1vmin);
     z-index: 1;
 }
 
-p {
-    text-shadow: 0px 0px 2px black;
-}
 </style>
 
-
-<div id="intro-text">
+<div id="intro-text" style="border-bottom: 4px solid {textColor};">
     {#if loaded}
-    <p transition:fade="{{ duration: 2000 }}">Welcome to Forest Stream Studios! Our site is still a work in progress, but you can follow our socials to keep in touch while we get things sorted here!</p>
+    <p style="color: {textColor}" transition:fade="{{ duration: 2000 }}">Welcome to Forest Stream Studios! Our site is still a work in progress, but you can follow our socials to keep in touch while we get things sorted here!</p>
     {/if}
 </div>
